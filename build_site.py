0f39a547c2f9627ddbf31fc2d3c6a05622b5428e
Christian Merten <christian@merten.dev>
2025-06-18 14:52:03 +0200

    basic css and layout

--- build_site.py
+++ build_site.py
@@ -1,15 +1,19 @@
 import yaml
 from staticjinja import Site
 
-def get_links():
-    with open('data/links.yml') as f:
+def load_yaml(name):
+    with open(f'data/{name}.yml') as f:
         return yaml.safe_load(f)
+
+context = {
+    'teaching': load_yaml('teaching'),
+    'seminars': load_yaml('seminars'),
+}
 
 site = Site.make_site(
     outpath='build',
     contexts=[
-        ('links.html', lambda: {'links': get_links()}),
-        ('index.html', lambda: {'links': get_links()}),
+        ('.*', lambda: {**context, **load_yaml('constants')}),
     ],
 )
 site.render()
